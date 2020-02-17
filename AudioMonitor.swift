12e0fa080dd994601e82bf4802232027577f8ca7
SilverWolf <argentwolf@protonmail.com>
2020-02-16 23:09:28 -0500

    Do FFTs with KissFFT library
    See https://github.com/mborgerding/kissfft.

--- AudioMonitor.swift
+++ AudioMonitor.swift
@@ -1,5 +1,6 @@
 import Foundation
 import PulseAudio
+import KissFFT
 
 class AudioMonitor {
 	
@@ -8,6 +9,8 @@ class AudioMonitor {
 	
 	private var pulseaudio: OpaquePointer? = nil
 	private var readQueue: DispatchQueue? = nil
+	
+	private var fftConfig: kiss_fft_cfg? = nil
 	
 	init() {
 		var sampleSpec = pa_sample_spec(format: PA_SAMPLE_S16LE, rate: 44100, channels: 1)
@@ -27,6 +30,8 @@ class AudioMonitor {
 			fputs("PulseAudio connection failed: \(String(cString: pa_strerror(errorID)))", stderr)
 			return
 		}
+		
+		initFFT()
 	}
 	
 	func startListening() {
@@ -41,16 +46,53 @@ class AudioMonitor {
 				if result < 0 {
 					fputs("PulseAudio read failed: \(String(cString: pa_strerror(errorID)))", stderr)
 				}
-				fputs("\(data)\n", stderr)
+				// fputs("\(data)\n", stderr)
+				
+				// perform FFT
+				self.doFFT(data: data)
+				
 				usleep(self.refreshTime)
 			}
 		}
+	}
+	
+	// FFT stuff //
+	
+	func initFFT() {
+	}
+	func deinitFFT() {
+	}
+	
+	func doFFT(data rawData: [UInt8]) {
+		fftConfig = kiss_fft_alloc(Int32(rawData.count), 0, nil, nil)
+		
+		let kissFFTIn: [kiss_fft_cpx] = rawData.map({ (n) in
+			return kiss_fft_cpx(r: Float(n), i: 0)
+		})
+		
+		var kissFFTOut = Array(repeating: kiss_fft_cpx(r: 0, i: 0), count: rawData.count)
+		kissFFTIn.withUnsafeBufferPointer { (fftIn) in
+			kissFFTOut.withUnsafeMutableBufferPointer { (fftOut) in
+				kiss_fft(fftConfig, fftIn.baseAddress, fftOut.baseAddress)
+			}
+		}
+		
+		let out = kissFFTOut.map({ (complex) in
+			return complex.r // real component
+		})
+		
+		print("\(out)")
+		
+		// maaybe ARC can handle this?
+		// free(fftConfig)
+		fftConfig = nil
 	}
 	
 	deinit {
 		// NOTE: This is NOT necessarily called when the program exits!
 		// The AudioMonitor has to be set to nil somewhere.
 		pa_simple_free(pulseaudio)
+		deinitFFT()
 	}
 	
 }
--- Makefile
+++ Makefile
@@ -3,10 +3,11 @@
 # vim:set fo=tcqr:
 #
 
-MODULEIMPORT=-I${PWD} libCursesUI.a -I${PWD}/modules/ncurses -I${PWD}/modules/pulseaudio
+MODULEIMPORT=-I${PWD} libCursesUI.a -I${PWD}/modules/ncurses -I${PWD}/modules/pulseaudio -I${PWD}/modules/kissfft
+OBJECTIMPORT=modules/kissfft/kissfft/kiss_fft.o
 
 pavisualizer: *.swift
-	swiftc -g -o pavisualizer *.swift ${MODULEIMPORT}
+	swiftc -g -o pavisualizer *.swift ${MODULEIMPORT} ${OBJECTIMPORT}
 
 clean:
 	rm -f pavisualizer
