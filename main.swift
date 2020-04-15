863d4636135f27b577b4c4adc55191a1066646e2
SilverWolf <argentwolf@protonmail.com>
2020-04-14 21:32:42 -0400

    Bring range back up to 0-6000 Hz
    It feels more responsive somehow at the higher sample rate.

--- main.swift
+++ main.swift
@@ -8,14 +8,14 @@ if CommandLine.arguments.count > 1 {
 
 let audioMonitor = AudioMonitor()
 audioMonitor.bufferSize = 1024*2
-audioMonitor.sampleRate = 44100 / 2
+// audioMonitor.sampleRate = 44100 / 2
 audioMonitor.refreshTime = UInt32(1_000_000/audioMonitor.sampleRate*audioMonitor.bufferSize/8)
 
 NCurses.initDisplay()
 
 let view = VisualizerView()
 
-view.smoothingWindow /= 2
+// view.smoothingWindow /= 2
 
 view.instantActions[" "] = { [unowned view] in
 	// view.logarithmic = !view.logarithmic
